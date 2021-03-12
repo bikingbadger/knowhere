e4a8d455ff29ebbd9954683540ef4e0d03d25bd3
Hilton Meyer <development@hiltonmeyer.com>
2021-03-12 19:15:09 +0200

    feat(user): Create register

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,10 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+  <v-app dark>
+    <v-app-bar app dark>
+      <v-app-bar-title>Knowhere</v-app-bar-title>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-btn text link to="/register">Register</v-btn>
     </v-app-bar>
-
     <v-main>
       <router-view />
     </v-main>
@@ -41,12 +13,12 @@
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
 
   components: {},
 
   data: () => ({
     //
-  }),
+  })
 };
 </script>
---
+++ src/views/Register.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="register">
+    <v-container>
+      <h1>Register</h1>
+      <v-form>
+        <v-text-field
+          label="Username"
+          type="text"
+          v-model="username"
+        ></v-text-field>
+        <v-text-field label="Email" type="email" v-model="email"></v-text-field>
+        <v-text-field
+          label="Password"
+          type="password"
+          v-model="password"
+        ></v-text-field>
+        <v-btn>Register</v-btn>
+      </v-form>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      email: "",
+      password: ""
+    };
+  }
+};
+</script>
